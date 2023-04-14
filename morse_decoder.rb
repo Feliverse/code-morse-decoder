8379d43a80fd2bafa9b23d9480fb8c2933f1ded5
Feliverse <haybar@gmail.com>
2023-04-14 14:50:15 -0400

    linters fixed

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,4 +1,4 @@
-CodeMorse = {
+CODE_MORSE = {
   '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
   '-..' => 'D', '.' => 'E', '..-.' => 'F',
   '--.' => 'G', '....' => 'H', '..' => 'I',
@@ -8,11 +8,11 @@ CodeMorse = {
   '...' => 'S', '-' => 'T', '..-' => 'U',
   '...-' => 'V', '.--' => 'W', '-..-' => 'X',
   '-.--' => 'Y', '--..' => 'Z'
-}
+}.freeze
 
 # Method to decode a Morse code character
 def decode_char(char)
-  CodeMorse[char]
+  CODE_MORSE[char]
 end
 
 # print (decode_char('.-')) + "\n"
@@ -22,27 +22,26 @@ end
 def decode_word(word)
   word_arr = word.split
   decode_word = ''
+
   word_arr.each do |char|
     decode_word += decode_char(char)
   end
+
   decode_word
 end
-
-# print (decode_word('-- -.--')) + "\n"
-# MY
 
 # Method to decode the entire message in Morse code
 def decode_message(message)
   string_arr = message.split('  ')
   decoded_message = ''
+
   string_arr.each do |word|
-    decoded_message += decode_word(word) + ' '
+    decoded_message += decode_word(word)
+    decoded_message += ' '
   end
+
   decoded_message.strip
 end
 
-# print (decode_message('-- -.--   -. .- -- .')) + "\n"
-# MY NAME
-
-print (decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')) + "\n"
+p(decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'))
 # A BOX FULL OF RUBIES
