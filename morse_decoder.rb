27f3bee1a5a4c405cbc78255dab568850049fd8f
Feliverse <haybar@gmail.com>
2023-04-14 14:34:17 -0400

    Update Gemfile

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,54 +1,48 @@
 CodeMorse = {
-   '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
-   '-..' => 'D', '.' => 'E', '..-.' => 'F',
-   '--.' => 'G', '....' => 'H', '..' => 'I',
-   '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
-   '--' => 'M', '-.' => 'N', '---' => 'O',
-   '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
-   '...' => 'S', '-' => 'T', '..-' => 'U',
-   '...-' => 'V', '.--' => 'W', '-..-' => 'X',
-   '-.--' => 'Y', '--..' => 'Z'
- }
- 
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
+  '-..' => 'D', '.' => 'E', '..-.' => 'F',
+  '--.' => 'G', '....' => 'H', '..' => 'I',
+  '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+  '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
+  '...' => 'S', '-' => 'T', '..-' => 'U',
+  '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+  '-.--' => 'Y', '--..' => 'Z'
+}
+
 # Method to decode a Morse code character
 def decode_char(char)
   CodeMorse[char]
 end
- 
+
 # print (decode_char('.-')) + "\n"
-  # A
+# A
 
 # Method to decode an entire word in Morse code
 def decode_word(word)
-  
   word_arr = word.split
   decode_word = ''
-  
   word_arr.each do |char|
     decode_word += decode_char(char)
   end
-
   decode_word
 end
 
 # print (decode_word('-- -.--')) + "\n"
-  # MY
+# MY
 
 # Method to decode the entire message in Morse code
 def decode_message(message)
   string_arr = message.split('  ')
   decoded_message = ''
-
   string_arr.each do |word|
     decoded_message += decode_word(word) + ' '
   end
-  
   decoded_message.strip
 end
 
 # print (decode_message('-- -.--   -. .- -- .')) + "\n"
-  # MY NAME
-
+# MY NAME
 
 print (decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')) + "\n"
 # A BOX FULL OF RUBIES
