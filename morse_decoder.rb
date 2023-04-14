5bf7907cf74c7954532a099f6b40c3c504d69351
Feliverse <haybar@gmail.com>
2023-04-14 10:24:58 -0400

    Add Method to decode a message

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -15,5 +15,33 @@
    CodeMorse[char]
  end
  
- print (decode_char('.-')) + "\n"
+ # print (decode_char('.-')) + "\n"
  # A
+
+ # Method to decode an entire word in Morse code
+def decode_word(word)
+  
+  word_arr = word.split
+  decode_word = ''
+  
+  word_arr.each do |char|
+    decode_word += decode_char(char)
+  end
+
+  decode_word
+end
+
+# Method to decode the entire message in Morse code
+def decode_message(message)
+  string_arr = message.split('  ')
+  decoded_message = ''
+
+  string_arr.each do |word|
+    decoded_message += decode_word(word) + ' '
+  end
+  
+  decoded_message.strip
+end
+
+print (decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')) + "\n"
+# A BOX FULL OF RUBIES
